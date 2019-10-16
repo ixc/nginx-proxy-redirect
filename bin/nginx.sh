335b177e57818237cc32826e2909e791f2992e19
Tai Lee <real.human@mrmachine.net>
2019-10-16 16:59:57 +1100

    Move example config out of data directory. Create actual config from example on first run to avoid errors.

--- bin/nginx.sh
+++ bin/nginx.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ ! -f /opt/nginx-proxy-redirect/data/config.yml ]]; then
+	cp /opt/nginx-proxy-redirect/config.yml.example /opt/nginx-proxy-redirect/data/config.yml
+fi
+
 export CONFIG="$(yq . /opt/nginx-proxy-redirect/data/config.yml)"
 dockerize -template /opt/nginx-proxy-redirect/etc/nginx.tmpl.conf:/opt/nginx-proxy-redirect/data/nginx.conf
 exec nginx -c /opt/nginx-proxy-redirect/data/nginx.conf "$@"
